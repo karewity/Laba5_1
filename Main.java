fb78aa4a64a5645157c0f70cda21d9be3eb401de
Шульга Виктория Васильевна <karewity@mail.ru>
2026-05-01 19:46:32 +0300

    Add project name variable

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ class Main{
     public static void main(String[] args){
         System.out.print("Это добавлено в ветке main");
         System.out.println("\nСтрока 2: работа продолжается");
+        String project = "Lab 5";
+        System.out.println("Проект: " + project);
     }
 }
