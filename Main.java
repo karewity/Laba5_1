a961c7f35d550e3bb93fe2b7826953c94298e07b
Шульга Виктория Васильевна <karewity@mail.ru>
2026-05-01 19:48:04 +0300

    Format output with println

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args){
-        System.out.print("Это добавлено в ветке main");
+        System.out.println("Это добавлено в ветке main");
         System.out.println("\nСтрока 2: работа продолжается");
         String project = "Lab 5";
         System.out.println("Проект: " + project);
