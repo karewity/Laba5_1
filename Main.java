e5a64812203c5a686e06e8e366a0dca279409a46
Шульга Виктория Васильевна <karewity@mail.ru>
2026-05-01 20:22:18 +0300

    Add step counter variable

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Работа в ветке dev запущена");
+        int step = 1;
+        System.out.println("Шаг выполнения: " + step);
     }
 }
