22610c706a3d6d63019deb930c94381eb13ff90f
Шульга Виктория Васильевна <karewity@mail.ru>
2026-05-01 19:47:11 +0300

    Add printInfo() method

--- Main.java
+++ Main.java
@@ -4,5 +4,9 @@ class Main{
         System.out.println("\nСтрока 2: работа продолжается");
         String project = "Lab 5";
         System.out.println("Проект: " + project);
+        printInfo();
+    }
+    static void printInfo() {
+        System.out.println(">>> Информация о работе");
     }
 }
