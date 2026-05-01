0a765208839139d2e8c9fecf0f7d263605f71721
Шульга Виктория Васильевна <karewity@mail.ru>
2026-05-01 20:23:24 +0300

    Add showBranchInfo() method

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ class Main {
         System.out.println("Работа в ветке dev запущена");
         int step = 1;
         System.out.println("Шаг выполнения: " + step);
+        showBranchInfo();
+
+    }
+    static void showBranchInfo() {
+        System.out.println(">>> Текущая ветка: dev");
     }
 }
